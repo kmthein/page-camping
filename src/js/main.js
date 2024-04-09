2b1dd2e5a1c9f5945859de234b5973fe0e13a3a3
kmthein <kmthein96.mm@gmail.com>
2024-04-09 06:53:56 +0630

    menu open

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import '../scss/style.scss';
+
+const overlay = document.querySelector(".l-head__overlay");
+
+const menuBtn = document.querySelector(".l-head__menu .l-head__menu__btn");
+menuBtn.addEventListener("click", () => {
+    overlay.classList.add("show");
+    document.body.classList.add("stopScroll");
+})
+const closeBtn = document.querySelector(".l-head__overlay .l-wrap-inner .close");
+closeBtn.addEventListener("click", () => {
+    overlay.classList.remove("show");
+    document.body.classList.remove("stopScroll");
+})
